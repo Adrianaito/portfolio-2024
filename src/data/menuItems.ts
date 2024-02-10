7163efb145ae6794d30136d30e8d79fc1015a767
Adriana Ito <ito.dri@gmail.com>
2024-02-10 11:46:19 +0900

    adds data for experience section, menu items and about section.

---
+++ src/data/menuItems.ts
@@ -0,0 +1,18 @@
+export const navItems = [
+  {
+    name: "about",
+    href: "#about",
+  },
+  {
+    name: "experience",
+    href: "#experience",
+  },
+  {
+    name: "projects",
+    href: "#projects",
+  },
+  {
+    name: "contact",
+    href: "/contact",
+  },
+];
