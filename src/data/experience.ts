aef34923ac020314d08f59c44767d5c9a72a73da
Adriana Ito <ito.dri@gmail.com>
2024-02-23 13:31:45 +0900

    updates experience data

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,24 +1,17 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 export const experience = [
-  {
-    company: "Sutrix",
-    link: "https://www.sutrix.com/",
-    position: "Software Engineer",
-    startDate: "2024",
-    endDate: "Present",
-    techStack: ["Javascript", "SCSS", "AEM", "Python", "Django"],
-    description: [
-      `I work as a Frontend Developer at Matinno. I am responsible for building and maintaining the user interface of the company's web applications. I also work closely with the design team to implement the UI/UX designs.`,
-    ],
-  },
   {
     company: "Matinno",
     link: "https://www.matinno.co/",
-    position: "Software Engineer (FreeLance)",
+    position: "Software Engineer",
     startDate: "2024",
     endDate: "Present",
     techStack: ["React", "Typescript", "NextJS", "Postgres", "Python", "Flask"],
     description: [
-      `I work as a Frontend Developer at Matinno. I am responsible for building and maintaining the user interface of the company's web applications. I also work closely with the design team to implement the UI/UX designs.`,
+      `As the Tech Lead for Backend Development at Matinno, a pioneering sustainable materials platform, I spearhead the engineering efforts to architect, develop, and deploy scalable and robust backend services. My role is instrumental in driving the platform's mission of reducing waste, enhancing design carbon accounting, and facilitating the recycling and upcycling of materials through technology. Leveraging my expertise in Flask, Python, and PostgreSQL, I have led my team in creating a high-performance API ecosystem that supports a wide array of functionalities, from material data management to user authentication and transaction processing.`,
+      `Key Responsibilities:`,
+      `- API Development and Integration: Designed and implemented RESTful APIs, enabling seamless interaction between the frontend application and the underlying database, enhancing the user experience and platform efficiency.`,
+      `- Database Design and Management: Oversaw the creation of a scalable PostgreSQL database schema that efficiently manages vast amounts of materials data, user profiles, and transaction histories, ensuring data integrity and rapid access.`,
     ],
   },
   {
@@ -33,6 +26,10 @@ export const experience = [
       `Spearheaded the creation of robust backend APIs using Python with the Flask framework, ensuring seamless data handling and efficient system functionality.`,
       `Utilized PostgreSQL for database needs, hosted on AWS RDS, to manage extensive user data and analytical metrics securely and efficiently.`,
       `Actively contributed to the frontend development using React, creating an intuitive and responsive user interface that seamlessly integrates with backend services.`,
+      `Key Responsibilities:`,
+      `- Spearheaded the creation of robust backend APIs using Python with the Flask framework, ensuring seamless data handling and efficient system functionality.`,
+      `- Database Management: Utilized PostgreSQL for database needs, hosted on AWS RDS, to manage extensive user data and analytical metrics securely and efficiently.`,
+      `- Deployment and Scalability: Responsible for deploying the application on AWS EC2 instances, emphasizing scalability and reliability in a cloud environment.`,
     ],
   },
   {
@@ -42,7 +39,10 @@ export const experience = [
     startDate: "2022",
     endDate: "Present",
     techStack: ["Ruby", "Javascript", "Postgres", "Rails"],
-    description: [`Description`],
+    description: [
+      `As a Volunteer Developer for the Audio Visual Calendar (AVC), a project dedicated to supporting children on the autism spectrum, I leverage my expertise in Ruby on Rails and JavaScript to enhance and debug a platform that transforms event planning into a fun and engaging process for the children. My journey with AVC began out of admiration for its mission and my personal connection to the project's owner, a batchmate from my programming bootcamp.`,
+      `Through my volunteer work with AVC, I have not only contributed to the technical advancement of the platform but have also played a role in supporting a project with a profound social impact.`,
+    ],
   },
   {
     company: "SBWorks",
@@ -51,6 +51,11 @@ export const experience = [
     startDate: "2021",
     endDate: "2022",
     techStack: ["React", "NextJS", "MySQL", "Flask", "Python"],
-    description: [`Description`],
+    description: [
+      `As a software engineer at SBworks, my initial role involved the development of a Minimum Viable Product (MVP) for an accounting firm. This project was my introduction to the fast-paced and challenging world of software development, providing me with a comprehensive learning experience.`,
+      `I was entrusted with both backend and frontend development responsibilities, ensuring a cohesive and functional application. This involved designing, implementing, and managing the application's overall architecture.`,
+      `Working closely with a senior developer, I navigated through the project's complexities, benefiting from guidance and support. This collaboration not only accelerated the development process but also enriched my problem-solving skills and technical knowledge.`,
+      `Achieved successful MVP deployment, directly contributing to the client's operational efficiency and service improvement.`,
+    ],
   },
 ];
