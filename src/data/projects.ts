8775a08af268d7bee65a1c169838e05001e81a11
Adriana Ito <ito.dri@gmail.com>
2024-05-23 15:25:10 +0900

    update projects data

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -41,15 +41,15 @@ export const projects = [
     thumbnail: "/medium/medium.png",
   },
 
-  {
-    name: "AWS static website",
-    description: [
-      "This website is hosted on AWS private S3 bucket and CloudFront.",
-    ],
-    techStack: ["AWS s3", "Cloudfront", "Route 53", "NextJS"],
-    link: "https://adriana-ito.link/",
-    thumbnail: "/AWS/static.png",
-  },
+  // {
+  //   name: "AWS static website",
+  //   description: [
+  //     "This website is hosted on AWS private S3 bucket and CloudFront.",
+  //   ],
+  //   techStack: ["AWS s3", "Cloudfront", "Route 53", "NextJS"],
+  //   link: "https://adriana-ito.link/",
+  //   thumbnail: "/AWS/static.png",
+  // },
   {
     name: "Portfolio v1",
     description: ["My previous portfolio website."],
