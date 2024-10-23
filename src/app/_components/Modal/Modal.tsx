9326b0d4c5e6b14529cf86d0ef66683ffbbad6e8
Adriana Ito <ito.dri@gmail.com>
2024-10-23 13:38:28 +0900

    create modal component

---
+++ src/app/_components/Modal/Modal.tsx
@@ -0,0 +1,84 @@
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import { cn } from "@lib/utils";
+
+interface ModalProps {
+  triggerText?: string;
+  triggerClassName?: string;
+  trigger?: React.ReactNode;
+  children: React.ReactNode;
+}
+
+export const ModalRoot: React.FC<ModalProps> = ({
+  triggerText = "Open Modal",
+  children,
+  triggerClassName,
+  trigger,
+  ...rest
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.body.style.overflow = "";
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.body.style.overflow = "";
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
+  return (
+    <>
+      {trigger ? (
+        <a
+          onClick={() => setIsOpen(true)}
+          className={cn(triggerClassName)}
+          {...rest}
+        >
+          {trigger}
+        </a>
+      ) : (
+        <button
+          onClick={() => setIsOpen(true)}
+          className={cn("btn-primary", triggerClassName)}
+          {...rest}
+        >
+          {triggerText}
+        </button>
+      )}
+      {/* <button
+        onClick={() => setIsOpen(true)}
+        className={cn("btn-primary", triggerClassName)}
+        {...rest}
+      >
+        {triggerText}
+      </button> */}
+      {isOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50">
+          <div
+            ref={modalRef}
+            className="rounded-lg p-2 w-full max-w-3xl shadow-lg relative"
+          >
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
